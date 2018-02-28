dbb5218abea1aaa4e0cd603df8fcf0a9b6c407af
Tony Atkins <tony@raisingthefloor.org>
2018-02-28 14:46:27 +0100

    Refined "missing record" script and templates.

---
+++ src/js/missing-products-by-source/csvEscapeHelper.js
@@ -0,0 +1,45 @@
+/*
+
+    A handlebars helper to escape text values used in a CSV file.  Only works in Node.js.  Implements the bare-minimum
+    escaping suggested here:
+
+    https://tools.ietf.org/html/rfc4180
+    https://stackoverflow.com/questions/769621/dealing-with-commas-in-a-csv-file
+
+    In short:
+
+    1. Carriage returns are replaced with spaces.
+    2. Double quotes are escaped by prepending another double-quote, per RFC 4180.
+
+ */
+"use strict";
+var fluid = require("infusion");
+var gpii  = fluid.registerNamespace("gpii");
+
+fluid.require("%gpii-handlebars");
+
+fluid.registerNamespace("gpii.ul.imports.helpers.csvEscape");
+
+gpii.ul.imports.helpers.csvEscape.csvEscapeString = function (string) {
+    var stringWithEscapedQuotes = string ? string.replace(/(["])/g, "\"\"") : string;
+    var stringWithoutReturns = stringWithEscapedQuotes ? stringWithEscapedQuotes.replace(/[\r\n]+/g," ") : stringWithEscapedQuotes;
+    return "\"" + stringWithoutReturns + "\"";
+};
+
+
+gpii.ul.imports.helpers.csvEscape.getCsvEscapeFunction = function () {
+    return function (rawString) {
+        return gpii.ul.imports.helpers.csvEscape.csvEscapeString(rawString);
+    };
+};
+
+fluid.defaults("gpii.ul.imports.helpers.csvEscape", {
+    gradeNames: ["gpii.handlebars.helper"],
+    helperName: "csvEscape",
+    invokers: {
+        "getHelper": {
+            "funcName": "gpii.ul.imports.helpers.csvEscape.getCsvEscapeFunction",
+            "args":     []
+        }
+    }
+});
---
+++ src/js/missing-products-by-source/index.js
@@ -0,0 +1,221 @@
+/*
+
+    A script to compare active unified records and report on sources that do not contain them.  Used to inform database
+    vendords of products they might not be aware of.
+
+*/
+/* eslint-env node */
+"use strict";
+var fluid = require("infusion");
+var gpii  = fluid.registerNamespace("gpii");
+
+fluid.registerNamespace("gpii.ul.imports.missingProductsBySource");
+
+require("../login");
+require("../launcher");
+require("../updates-report/lib/renderer");
+require("./csvEscapeHelper");
+
+var fs            = require("fs");
+var nodemailer    = require("nodemailer");
+var os            = require("os");
+var path          = require("path");
+var request       = require("request");
+var smtpTransport = require("nodemailer-smtp-transport");
+
+fluid.require("%ul-api");
+
+gpii.ul.imports.missingProductsBySource.login = function (that) {
+    var loginPromise = gpii.ul.imports.login(that);
+    loginPromise.then(that.retrieveUnifiedRecords, fluid.fail);
+};
+
+gpii.ul.imports.missingProductsBySource.retrieveUnifiedRecords = function (that) {
+    fluid.log("Looking up all existing products...");
+    var requestOptions = {
+        url:  that.options.urls.products + "?&limit=1000000",
+        json: true,
+        jar:  true
+    };
+    request.get(requestOptions, that.processUnifiedRecords);
+};
+
+gpii.ul.imports.missingProductsBySource.processUnifiedRecords = function (that, error, response, body) {
+    var runTimestamp = (new Date()).toISOString();
+    var records = fluid.get(body, "products");
+    var missingRecordsBySource = {};
+    fluid.each(records, function (record) {
+        if (record.status === "active") {
+            var recordSources = fluid.transform(record.sources, function (sourceRecord) { return sourceRecord.source; });
+            fluid.each(that.options.sources, function (source) {
+                if (recordSources.indexOf(source) === -1) {
+                    var existingEntries = fluid.get(missingRecordsBySource, source);
+                    if (existingEntries !== undefined) {
+                        existingEntries.push(record);
+                    }
+                    else {
+                        fluid.set(missingRecordsBySource, source, [record]);
+                    }
+                }
+            });
+        }
+    });
+
+    var emailPromises = [];
+
+    fs.writeFileSync(path.resolve("/tmp", "missingRecords-" + runTimestamp + ".json"), JSON.stringify(missingRecordsBySource, null, 2), "utf8");
+
+
+    var missingRecordCountsAllSources = {};
+    fluid.each(missingRecordsBySource, function (missingRecords, sourceKey) {
+        // We need to distinguish records that are only in the UL and not also in a vendor, which can't be done purely in handlebars.
+        var recordCountBySource = {};
+        var taggedRecords = fluid.transform(missingRecords, function (missingRecord) {
+            var distinctSourceHash = {};
+            fluid.each(missingRecord.sources, function (sourceRecord) {
+                var sourceName = fluid.get(gpii.ul.api.sources.sources, [sourceRecord.source, "name"]) || sourceRecord.source;
+                if (sourceRecord.status !== "deleted") {
+                    distinctSourceHash[sourceName] = sourceRecord;
+                }
+            });
+            var distinctSources = Object.keys(distinctSourceHash);
+
+            var hasNonSaiRecord = fluid.find(distinctSources, function (key) {
+                if (key !== "Unified Listing") { return true; }
+            });
+
+            fluid.each(distinctSources, function (source) {
+                if (source !== "Unified Listing" || hasNonSaiRecord === undefined) {
+                    if (recordCountBySource[source]) {
+                        recordCountBySource[source]++;
+                    }
+                    else {
+                        recordCountBySource[source] = 1;
+                    }
+                }
+            });
+
+            return fluid.merge({}, missingRecord, { onlyInUL: !hasNonSaiRecord });
+        });
+        missingRecordCountsAllSources[sourceKey] = recordCountBySource;
+
+        var promise = fluid.promise();
+        emailPromises.push(promise);
+        var transport = nodemailer.createTransport(smtpTransport(that.options.transportOptions));
+
+        var sourceName = fluid.get(gpii.ul.api.sources.sources, [sourceKey, "name"]) || sourceKey;
+
+        var mailOptions = fluid.copy(that.options.baseMailOptions);
+
+        mailOptions.subject = fluid.stringTemplate(that.options.stringTemplates.mailSubject, { source: sourceKey });
+        mailOptions.text = that.renderer.render(that.options.templateKeys.sourceEmailText, { options: that.options, source: sourceName, recordCountBySource: recordCountBySource, missingRecords: taggedRecords});
+        mailOptions.html = that.renderer.render(that.options.templateKeys.sourceEmailHtml, { options: that.options, source: sourceName, recordCountBySource: recordCountBySource, missingRecords: taggedRecords});
+
+
+        var sourceCSV   = that.renderer.render(that.options.templateKeys.sourceCSV, { options: that.options, source: sourceName, missingRecords: taggedRecords});
+        var attachmentFilename = "ul-records-missing-source-data-" + sourceKey + "-" + runTimestamp + ".csv";
+        mailOptions.attachments = [{
+            filename:    attachmentFilename,
+            content:     sourceCSV,
+            contentType: "text/csv"
+        }];
+        transport.sendMail(mailOptions, function (err, info) {
+            if (err) {
+                // TODO: Consider saving failures to a retry file of some kind...
+                promise.reject(err);
+            }
+            else {
+                promise.resolve(info);
+            }
+        });
+    });
+    var sequence = fluid.promise.sequence(emailPromises);
+    sequence.then(function (result) {
+        fluid.log("Sent ", result.length, " database vendor emails.");
+
+        var summaryContent = that.renderer.render(that.options.templateKeys.overallSummary, missingRecordCountsAllSources);
+
+        var summaryFilename = "ul-records-missing-source-data-summary-" + runTimestamp + ".html";
+        var summaryPath = path.resolve(os.tmpdir(), summaryFilename);
+
+        fs.writeFileSync(summaryPath, summaryContent, "utf8");
+        fluid.log("Save summary report to '", summaryPath, "'.");
+    }, fluid.fail);
+};
+
+fluid.defaults("gpii.ul.imports.missingProductsBySource", {
+    gradeNames: ["fluid.component"],
+    // TODO: Confirm the actual details.
+    stringTemplates: {
+        mailSubject: "Unified Listing entries that we cannot link to your database (%source)."
+    },
+    baseMailOptions: {
+        from: "noreply@raisingthefloor.org",
+        // Mail to tony@raisingthefloor.org for now and confirm the final email addresses at the meeting.
+        to: "tony@raisingthefloor.org"
+    },
+    sources: [
+        "ATAust",
+        "Dlf data",
+        "EASTIN Admin",
+        "Handicat",
+        "Hjælpemiddelbasen",
+        "Hulpmiddelenwijzer",
+        "Oaeg",
+        "Rehadat",
+        "Siva",
+        "Vlibank"
+    ],
+    templateKeys: {
+        overallSummary:  "missing-products-overall-summary",
+        sourceCSV:       "missing-products-by-source-CSV",
+        sourceEmailHtml: "missing-products-by-source-email-html",
+        sourceEmailText: "missing-products-by-source-email-text"
+    },
+    listeners: {
+        "onCreate.login": {
+            funcName: "gpii.ul.imports.missingProductsBySource.login",
+            args:     ["{that}"]
+        }
+    },
+    invokers: {
+        "processUnifiedRecords": {
+            funcName: "gpii.ul.imports.missingProductsBySource.processUnifiedRecords",
+            args: ["{that}", "{arguments}.0", "{arguments}.1", "{arguments}.2"]
+        },
+        "retrieveUnifiedRecords": {
+            funcName: "gpii.ul.imports.missingProductsBySource.retrieveUnifiedRecords",
+            args:     ["{that}"]
+        }
+    },
+    components: {
+        renderer: {
+            type: "gpii.ul.imports.renderer",
+            options: {
+                components: {
+                    csvEscape: {
+                        type: "gpii.ul.imports.helpers.csvEscape"
+                    }
+                }
+            }
+        }
+    }
+});
+
+fluid.defaults("gpii.ul.imports.curation.missingProductsBySource.launcher", {
+    gradeNames:  ["gpii.ul.imports.launcher"],
+    optionsFile: "%ul-imports/configs/reports-missing-products-by-source-prod.json",
+    filterKeys: true,
+    "yargsOptions": {
+        "describe": {
+            "username":   "The username to use when reading records from the UL.",
+            "password":   "The password to use when reading records from the UL.",
+            "setLogging": "The logging level to use.  Set to `true` by default."
+        },
+        "coerce": {
+            "setLogging": JSON.parse
+        }
+    }
+});
+
+gpii.ul.imports.curation.missingProductsBySource.launcher();
