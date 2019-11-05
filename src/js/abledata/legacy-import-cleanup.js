f747aead11abd93d1e7d2d858f543996c4cb3972
Tony Atkins <tony@raisingthefloor.org>
2019-11-05 14:17:13 +0100

    Added legacy AbleData import prep and cleanup scripts.

---
+++ src/js/abledata/legacy-import-cleanup.js
@@ -0,0 +1,18 @@
+/* Copy a canned import into the cache. */
+"use strict";
+var fluid = require("infusion");
+fluid.setLogging(true);
+
+var os    = require("os");
+var fs    = require("fs");
+var path  = require("path");
+
+var cacheFilePath = path.resolve(os.tmpdir(), "ableData.xml");
+
+if (fs.existsSync(cacheFilePath)) {
+    fs.unlinkSync(cacheFilePath);
+    fluid.log("Cache file removed.");
+}
+else {
+    fluid.log("No cache file found, nothing to do.");
+}
---
+++ src/js/abledata/legacy-import-prep.js
@@ -0,0 +1,18 @@
+/* Copy a canned import into the cache. */
+"use strict";
+var fluid = require("infusion");
+fluid.setLogging(true);
+
+var os    = require("os");
+var fs    = require("fs");
+var path  = require("path");
+
+require("../../../");
+
+var toCopyPath = fluid.module.resolvePath("%ul-imports/data/abledata-legacy-data/all-discontinued-products.xml");
+var cacheFilePath = path.resolve(os.tmpdir(), "ableData.xml");
+
+var content = fs.readFileSync(toCopyPath, { encoding: "utf8"});
+fs.writeFileSync(cacheFilePath, content);
+
+fluid.log("Cache file copied.");
